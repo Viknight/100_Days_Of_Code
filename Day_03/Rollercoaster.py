0b3f1a713027a474b3cfcf0fb5f7109a26aeb3a0
Varun Yadav <41319713+Viknight@users.noreply.github.com>
2021-07-07 21:48:10 +0530

    Day 3 codes

---
+++ Day_03/Rollercoaster.py
@@ -0,0 +1,11 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if (height > 120):
+	Age = int(input('What is your age?'))
+	if Age > 18:
+		print('Please pay 12$')
+	else:
+		print('Please pay 7$')
+  print('You can go ahead and enjoy your ride')
+else:
+  print('Sorry, you cannot proceed')
