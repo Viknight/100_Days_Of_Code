0f6ff12504843a9bf16658407f038c51f9a13cdb
Varun Yadav <varunyadav155@gmail.com>
2021-07-14 21:26:44 +0530

    Day_06 commits

---
+++ Day_06/Reeborgs_world_hurdle_1.py
@@ -0,0 +1,19 @@
+#www.reeborg.ca
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+for i in range(6):
+    jump()
+    
