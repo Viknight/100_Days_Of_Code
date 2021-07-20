67b965955b9fe0eb580c81a2a2eb17433ae4fdcb
Varun Yadav <varunyadav155@gmail.com>
2021-07-20 11:39:41 +0530

    Day_08 functions

---
+++ Day_08/Functions_with _input.py
@@ -0,0 +1,17 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet():
+  print("Hello")
+  print("world")
+  print("Welcome")
+
+def greet_with(name,location):
+  print(f"Hello {name}")
+  print(f"How is the weather at {location}?")
+
+greet_with("varun","mumbai")
+
+greet_with(location="mumbai", name =" varun") 
